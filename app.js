0ca4d4971755a029fcfa135920330b987a21f510
Sebastian <sebastianrichel18@gmail.com>
2021-08-24 17:00:33 -0400

    Block Class

--- app.js
+++ app.js
@@ -8,4 +8,25 @@ function getHTML(url) {
     });
 }
 
-getHTML("https://google.com")
+function Block(str) {
+    let endOfOpenTagIndex = str.indexOf(">")
+    startTag = str.substr(1, endOfOpenTagIndex) // get the start tag (in between opening < >)
+    let attributes = startTag.split(" ")
+    attributes.pop() // remove closing > or />
+    this.type = attributes.shift() // get name of tag, then remove from attr list
+    this.attr = {}
+    for (let word of attributes) {
+        let words = word.split("=")
+        let key = words[0]
+        let val = words[1]
+        if(words[2]) val += '='+words[2]; // edge case where val is key val pair
+        this.attr[key] = val
+    }
+    this.inner = str.substring(endOfOpenTagIndex + 1).substring(0, str.length - (((this.type.length + 2) * 2)) - 1);
+}
+
+let block2 = new Block('<meta name="robots" content="index,follow">')
+console.log(block2, '\n')
+
+let block1 = new Block("<meta http-equiv='X-UA-Compatible' content='IE=edge' />")
+console.log(block1)
